a5e48d4d6fef3b17b327d709fa91598c20261831
Min-Ji Kim <62365297+MIN-04@users.noreply.github.com>
2022-10-24 21:21:12 +0900

    Create no12925.py

---
+++ Level/Level01/no12925.py
@@ -0,0 +1,9 @@
+# LEVEL 01 - 12925. 문자열을 정수로 바꾸기 (https://school.programmers.co.kr/learn/courses/30/lessons/12925)
+
+def solution(s):
+    if s[0] == '+':
+        return +int(s[1:])
+    elif s[0] == '-':
+        return -int(s[1:])
+    else:
+        return int(s)
