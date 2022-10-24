5494a29b62df76a157de30276d516cbcff6a3124
Min-Ji Kim <62365297+MIN-04@users.noreply.github.com>
2022-10-24 20:09:21 +0900

    Create no12937.py

---
+++ Level/Level01/no12937.py
@@ -0,0 +1,7 @@
+# LEVEL 01 - 12937. 짝수와 홀수 (https://school.programmers.co.kr/learn/courses/30/lessons/12937)
+
+def solution(num):
+    if num % 2:
+        return 'Odd'
+    else:
+        return 'Even'
