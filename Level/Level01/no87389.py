13f37d6ee85ab50825cce97795f5dc704db2af1c
Min-Ji Kim <62365297+MIN-04@users.noreply.github.com>
2022-10-24 21:25:36 +0900

    Create no87389.py

---
+++ Level/Level01/no87389.py
@@ -0,0 +1,6 @@
+# LEVEL 01 - 87389. 나머지가 1이 되는 수 찾기 (https://school.programmers.co.kr/learn/courses/30/lessons/87389)
+
+def solution(n):
+    for x in range(1, n+1):
+        if n % x == 1:
+            return x
